5bda513047a4c849435e31b953a2cd7ed72a5b11
mini-jail <mini-jail@proton.me>
2023-02-22 17:50:54 +0100

    oops?

--- mod.ts
+++ mod.ts
@@ -1,19 +1,3 @@
-const VALUE = 0
-const NODE_PARENT = 1
-const NODE_CHILDREN = 2
-const NODE_CONTEXT = 3
-const NODE_CLEANUPS = 4
-const NODE_CALLBACK = 5
-const NODE_SOURCES = 6
-const NODE_SOURCESLOTS = 7
-const SOURCE_NODES = 1
-const SOURCE_NODESLOTS = 2
-const Error = Symbol("Error")
-const Queue = new Set<Node>()
-
-let updateQueue: Set<Node> | undefined
-let parentNode: Node | undefined
-
 export type Cleanup = () => void
 export type Callback<T = void> = T extends void ? (() => void)
   : ((value: T) => T)
@@ -24,19 +8,19 @@ export type Signal<T = any> = {
   (callback: Callback<T>): void
 }
 export type Source<T = any> = {
-  [VALUE]?: T
-  [SOURCE_NODES]?: Node[]
-  [SOURCE_NODESLOTS]?: number[]
+  value?: T
+  nodes?: Node[]
+  nodeSlots?: number[]
 }
 export type Node<T = any> = {
-  [VALUE]?: T
-  [NODE_PARENT]?: Node
-  [NODE_CHILDREN]?: Node[]
-  [NODE_CONTEXT]?: { [id: symbol]: any }
-  [NODE_CLEANUPS]?: Cleanup[]
-  [NODE_CALLBACK]?: Callback<any>
-  [NODE_SOURCES]?: Source[]
-  [NODE_SOURCESLOTS]?: number[]
+  value?: T
+  parentNode?: Node
+  children?: Node[]
+  context?: { [id: symbol]: any }
+  cleanups?: Cleanup[]
+  callback?: Callback<any>
+  sources?: Source[]
+  sourceSlots?: number[]
 }
 export type Ref<T = any> = {
   value: T
@@ -46,6 +30,11 @@ export type Context<T> = {
   readonly defaultValue: T | undefined
   provide<R>(value: T, callback: Accessor<R>): R
 }
+
+const Error = Symbol("Error")
+const Queue = new Set<Node>()
+let nodeQueue: Set<Node> | undefined
+let parentNode: Node | undefined
 
 export function root<T = any>(callback: (cleanup: Cleanup) => T): T | void {
   const _node = node<T>()
@@ -61,26 +50,22 @@ export function root<T = any>(callback: (cleanup: Cleanup) => T): T | void {
   } catch (error) {
     handleError(error)
   } finally {
-    parentNode = _node[NODE_PARENT]
+    parentNode = _node.parentNode
   }
 }
 
-function node<T = any>(initialValue?: T): Node<T> {
-  const _node: Node<T> = [initialValue]
+function node<T = any>(initialValue?: T, callback?: Callback<T>): Node<T> {
+  const _node: Node<T> = {}
+  if (initialValue) _node.value = initialValue
+  if (callback) _node.callback = callback
   if (parentNode) {
-    _node[NODE_PARENT] = parentNode
-    if (parentNode[NODE_CHILDREN] === undefined) {
-      parentNode[NODE_CHILDREN] = [_node]
+    _node.parentNode = parentNode
+    if (parentNode.children === undefined) {
+      parentNode.children = [_node]
     } else {
-      parentNode[NODE_CHILDREN].push(_node)
+      parentNode.children.push(_node)
     }
   }
-  return _node
-}
-
-function computation<T>(callback: Callback<T>, initialValue?: T): Node<T> {
-  const _node = node<T>(initialValue)
-  _node[NODE_CALLBACK] = callback
   return _node
 }
 
@@ -89,7 +74,7 @@ export function computed<T>(
   initialValue?: T,
 ): Accessor<T> {
   const _source = source(initialValue)
-  effect(() => sourceValue(_source, callback(_source[VALUE]!)))
+  effect(() => sourceValue(_source, callback(_source.value!)))
   return () => sourceValue(_source)
 }
 
@@ -106,8 +91,8 @@ export function effect<T>(callback: Callback<T>): void
 export function effect<T>(callback: Callback<T>, initialValue: T): void
 export function effect(callback: Callback<any>, initialValue?: any): void {
   if (parentNode) {
-    const _node = computation(callback, initialValue)
-    if (updateQueue) updateQueue.add(_node)
+    const _node = node(initialValue, callback)
+    if (nodeQueue) nodeQueue.add(_node)
     else queueMicrotask(() => updateNode(_node, false))
   } else {
     queueMicrotask(() => callback(initialValue))
@@ -116,14 +101,16 @@ export function effect(callback: Callback<any>, initialValue?: any): void {
 
 function lookup(node: Node | undefined, id: symbol): any | undefined {
   return node
-    ? node[NODE_CONTEXT] && id in node[NODE_CONTEXT]
-      ? node[NODE_CONTEXT][id]
-      : lookup(node[NODE_PARENT], id)
+    ? node.context && id in node.context
+      ? node.context[id]
+      : lookup(node.parentNode, id)
     : undefined
 }
 
 function source<T = any>(initialValue?: T): Source<T> {
-  return [initialValue]
+  const _source: Source<T> = {}
+  if (initialValue) _source.value = initialValue
+  return _source
 }
 
 function sourceValue<T = any>(source: Source<T>): T
@@ -134,34 +121,34 @@ function sourceValue<T = any>(
 ): void
 function sourceValue<T = any>(source: Source<T>, next?: any) {
   if (arguments.length === 1) {
-    if (parentNode && parentNode[NODE_CALLBACK]) {
-      const sourceSlot = source[SOURCE_NODES]?.length || 0,
-        nodeSlot = parentNode[NODE_SOURCES]?.length || 0
-      if (parentNode[NODE_SOURCES] === undefined) {
-        parentNode[NODE_SOURCES] = [source]
-        parentNode[NODE_SOURCESLOTS] = [sourceSlot]
+    if (parentNode && parentNode.callback) {
+      const sourceSlot = source.nodes?.length || 0,
+        nodeSlot = parentNode.sources?.length || 0
+      if (parentNode.sources === undefined) {
+        parentNode.sources = [source]
+        parentNode.sourceSlots = [sourceSlot]
       } else {
-        parentNode[NODE_SOURCES].push(source)
-        parentNode[NODE_SOURCESLOTS]!.push(sourceSlot)
+        parentNode.sources.push(source)
+        parentNode.sourceSlots!.push(sourceSlot)
       }
-      if (source[SOURCE_NODES] === undefined) {
-        source[SOURCE_NODES] = [parentNode]
-        source[SOURCE_NODESLOTS] = [nodeSlot]
+      if (source.nodes === undefined) {
+        source.nodes = [parentNode]
+        source.nodeSlots = [nodeSlot]
       } else {
-        source[SOURCE_NODES]!.push(parentNode)
-        source[SOURCE_NODESLOTS]!.push(nodeSlot)
+        source.nodes!.push(parentNode)
+        source.nodeSlots!.push(nodeSlot)
       }
     }
-    return source[VALUE]
+    return source.value
   }
   if (typeof next === "function") {
-    next = next(source[VALUE])
+    next = next(source.value)
   }
-  source[VALUE] = next
-  if (source[SOURCE_NODES]?.length) {
+  source.value = next
+  if (source.nodes?.length) {
     batch(() => {
-      for (const node of source[SOURCE_NODES]!) {
-        updateQueue!.add(node)
+      for (const node of source.nodes!) {
+        nodeQueue!.add(node)
       }
     })
   }
@@ -194,17 +181,17 @@ function handleError(error: any): void {
 
 export function error<T = any>(callback: Callback<T>): void {
   if (parentNode === undefined) return
-  if (!parentNode[NODE_CONTEXT]) {
-    parentNode[NODE_CONTEXT] = { [Error]: [callback] }
+  if (parentNode.context === undefined) {
+    parentNode.context = { [Error]: [callback] }
   } else {
-    parentNode[NODE_CONTEXT][Error].push(callback)
+    parentNode.context[Error].push(callback)
   }
 }
 
 export function cleanup(callback: Callback): void {
   if (parentNode === undefined) return
-  else if (!parentNode[NODE_CLEANUPS]) parentNode[NODE_CLEANUPS] = [callback]
-  else parentNode[NODE_CLEANUPS].push(callback)
+  else if (!parentNode.cleanups) parentNode.cleanups = [callback]
+  else parentNode.cleanups.push(callback)
 }
 
 export function untrack<T>(callback: () => T): T {
@@ -216,29 +203,29 @@ export function untrack<T>(callback: () => T): T {
 }
 
 function batch<T>(callback: () => T): T {
-  if (updateQueue) return callback()
-  updateQueue = Queue
+  if (nodeQueue) return callback()
+  nodeQueue = Queue
   const result = callback()
   queueMicrotask(flush)
   return result
 }
 
 function flush(): void {
-  if (updateQueue === undefined) return
-  for (const node of updateQueue) {
-    updateQueue.delete(node)
+  if (nodeQueue === undefined) return
+  for (const node of nodeQueue) {
+    nodeQueue.delete(node)
     updateNode(node, false)
   }
-  updateQueue = undefined
+  nodeQueue = undefined
 }
 
 function updateNode(node: Node, complete: boolean): void {
   cleanNode(node, complete)
-  if (node[NODE_CALLBACK] === undefined) return
+  if (node.callback === undefined) return
   const previousNode = parentNode
   parentNode = node
   try {
-    node[VALUE] = node[NODE_CALLBACK](node[0])
+    node.value = node.callback(node.value)
   } catch (error) {
     handleError(error)
   } finally {
@@ -248,50 +235,47 @@ function updateNode(node: Node, complete: boolean): void {
 
 function cleanNodeSources(node: Node): void {
   let source: Source, sourceSlot: number, sourceNode: Node, nodeSlot: number
-  while (node[NODE_SOURCES]!.length) {
-    source = node[NODE_SOURCES]!.pop()!
-    sourceSlot = node[NODE_SOURCESLOTS]!.pop()!
-    if (source[SOURCE_NODES]?.length) {
-      sourceNode = source[SOURCE_NODES].pop()!
-      nodeSlot = source[SOURCE_NODESLOTS]!.pop()!
-      if (sourceSlot < source[SOURCE_NODES].length) {
-        source[SOURCE_NODES][sourceSlot] = sourceNode
-        source[SOURCE_NODESLOTS]![sourceSlot] = nodeSlot
-        sourceNode[NODE_SOURCESLOTS]![nodeSlot] = sourceSlot
+  while (node.sources!.length) {
+    source = node.sources!.pop()!
+    sourceSlot = node.sourceSlots!.pop()!
+    if (source.nodes?.length) {
+      sourceNode = source.nodes.pop()!
+      nodeSlot = source.nodeSlots!.pop()!
+      if (sourceSlot < source.nodes.length) {
+        source.nodes[sourceSlot] = sourceNode
+        source.nodeSlots![sourceSlot] = nodeSlot
+        sourceNode.sourceSlots![nodeSlot] = sourceSlot
       }
     }
   }
 }
 
 function cleanChildNodes(node: Node, complete: boolean): void {
-  const hasCallback = node[NODE_CALLBACK] !== undefined
+  const hasCallback = node.callback !== undefined
   let childNode: Node
-  while (node[NODE_CHILDREN]!.length) {
-    childNode = node[NODE_CHILDREN]!.pop()!
+  while (node.children!.length) {
+    childNode = node.children!.pop()!
     cleanNode(
       childNode,
-      complete || (hasCallback && childNode[NODE_CALLBACK] !== undefined),
+      complete || (hasCallback && childNode.callback !== undefined),
     )
   }
 }
 
 function cleanNode(node: Node, complete: boolean): void {
-  if (node[NODE_SOURCES]?.length) cleanNodeSources(node)
-  if (node[NODE_CHILDREN]?.length) cleanChildNodes(node, complete)
-  if (node[NODE_CLEANUPS]?.length) {
-    while (node[NODE_CLEANUPS].length) {
-      node[NODE_CLEANUPS].pop()!()
+  if (node.sources?.length) cleanNodeSources(node)
+  if (node.children?.length) cleanChildNodes(node, complete)
+  if (node.cleanups?.length) {
+    while (node.cleanups.length) {
+      node.cleanups.pop()!()
     }
   }
-  delete node[NODE_CONTEXT]
+  node.context = undefined
   if (complete) {
-    delete node[VALUE]
-    delete node[NODE_PARENT]
-    delete node[NODE_CHILDREN]
-    delete node[NODE_CLEANUPS]
-    delete node[NODE_CALLBACK]
-    delete node[NODE_SOURCES]
-    delete node[NODE_SOURCESLOTS]
+    for (const property in node) {
+      node[property as keyof Node] = undefined
+      delete node[property as keyof Node]
+    }
   }
 }
 
@@ -311,7 +295,7 @@ export function provide<T, R>(
   callback: Accessor<R>,
 ): R {
   return root(() => {
-    parentNode![NODE_CONTEXT] = { [context.id]: value }
+    parentNode!.context = { [context.id]: value }
     return callback()
   })!
 }
@@ -319,5 +303,3 @@ export function provide<T, R>(
 export function use<T>(context: Context<T>): T {
   return lookup(parentNode, context.id) || context.defaultValue
 }
-
-context.use = use
