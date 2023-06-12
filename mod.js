fea61d156db6a298e26f923f954752f841f22806
mini-jail <mini-jail@proton.me>
2023-06-12 15:58:16 +0200

    ehehehe

---
+++ mod.js
@@ -0,0 +1,538 @@
+/**
+ * @typedef {() => void} Cleanup
+ */
+
+/**
+ * @template [T = unknown]
+ * @typedef {{
+ *   () : T
+ *   (value: T): void
+ *   (callback: (currentValue: T | undefined) => T): void
+ * }} Signal<T>
+ */
+
+/**
+ * @template [T = unknown]
+ * @typedef {{
+ *   value: T | undefined | null
+ *   nodes: Node[] | null
+ *   nodeSlots: number[] | null
+ * }} Source<T>
+ */
+
+/**
+ * @template [T = unknown]
+ * @typedef {{
+ *   value: T | undefined | null
+ *   parentNode: Node | null
+ *   children: Node[] | null
+ *   injections: { [id: symbol]: any } | null
+ *   cleanups: Cleanup[] | null
+ *   callback: ((currentValue: T | undefined) => T) | null
+ *   sources: Source[] | null
+ *   sourceSlots: number[] | null
+ * }} Node<T>
+ */
+
+/**
+ * @template [T = unknown]
+ * @typedef {{ value: T }} Ref<T>
+ */
+
+/**
+ * @template [T = unknown]
+ * @typedef {{
+ *   readonly id: symbol
+ *   readonly defaultValue: T | undefined
+ *   provide<R>(value: T, callback: () => R): R
+ * }} Injection<T>
+ */
+
+const Error = Symbol();
+/** @type {Set<Node>} */
+const Queue = new Set();
+let isRunning = false;
+/** @type {Node | null} */
+let activeNode = null;
+
+/**
+ * @template [T = unknown]
+ * @param {(cleanup: Cleanup) => T | void} callback
+ * @returns {T | void}
+ */
+export function scoped(callback) {
+  const node = activeNode = createNode();
+  try {
+    return batch(() =>
+      callback(
+        callback.length === 0 ? undefined : cleanNode.bind(node, true),
+      )
+    );
+  } catch (error) {
+    handleError(error);
+  } finally {
+    activeNode = node.parentNode;
+  }
+}
+
+/**
+ * @returns {Node | null}
+ */
+export function nodeRef() {
+  return activeNode;
+}
+
+/**
+ * @template [T = unknown]
+ * @param {Node} node
+ * @param {() => T} callback
+ */
+export function withNode(node, callback) {
+  const previousNode = activeNode;
+  activeNode = node;
+  const result = callback();
+  activeNode = previousNode;
+  return result;
+}
+
+/**
+ * @template [T = unknown]
+ * @param {T} [initialValue]
+ * @param {(currentValue: T | undefined) => T} [callback]
+ * @returns {Node<T>}
+ */
+function createNode(initialValue, callback) {
+  /** @type {Node<T>} */
+  const node = {
+    value: initialValue,
+    parentNode: activeNode,
+    children: null,
+    injections: null,
+    cleanups: null,
+    callback: callback || null,
+    sources: null,
+    sourceSlots: null,
+  };
+  if (activeNode !== null) {
+    if (activeNode.children === null) {
+      activeNode.children = [node];
+    } else {
+      activeNode.children.push(node);
+    }
+  }
+  return node;
+}
+
+/**
+ * @param {() => void} callback
+ * @returns {void}
+ */
+export function onMount(callback) {
+  effect(() => untrack(callback));
+}
+
+/**
+ * @param {() => void} callback
+ * @returns {void}
+ */
+export function onDestroy(callback) {
+  onCleanup(() => untrack(callback));
+}
+
+/**
+ * @template [T = unknown]
+ * @param {() => void} dependency
+ * @param {(currentValue: T | undefined) => T} callback
+ * @returns {(currentValue: T | undefined) => T}
+ */
+export function on(dependency, callback) {
+  return ((currentValue) => {
+    dependency();
+    return untrack(() => callback(currentValue));
+  });
+}
+
+/**
+ * @template [T = unknown]
+ * @param {(currentValue: T | undefined) => T} callback
+ * @param {T} [initialValue]
+ * @returns {void}
+ */
+export function effect(callback, initialValue) {
+  if (activeNode !== null) {
+    const node = createNode(initialValue, callback);
+    if (isRunning) {
+      Queue.add(node);
+    } else {
+      queueMicrotask(() => updateNode.call(node, false));
+    }
+  } else {
+    queueMicrotask(() => callback(initialValue));
+  }
+}
+
+/**
+ * @template [T = unknown]
+ * @param {(currentValue: T | undefined) => T} callback
+ * @param {T} [initialValue]
+ * @returns {() => T}
+ */
+export function computed(callback, initialValue) {
+  const source = createSource(initialValue);
+  effect(() => setSourceValue.call(source, callback(source.value)));
+  return getSourceValue.bind(source);
+}
+
+/**
+ * @this {Node | null}
+ * @param {symbol} id
+ * @returns {any | undefined}
+ */
+function lookup(id) {
+  return this !== null
+    ? this.injections !== null && id in this.injections
+      ? this.injections[id]
+      : lookup.call(this.parentNode, id)
+    : undefined;
+}
+
+/**
+ * @template [T = unknown]
+ * @param {T} [initialValue]
+ * @returns {Source<T>}
+ */
+function createSource(initialValue) {
+  return { value: initialValue, nodes: null, nodeSlots: null };
+}
+
+/**
+ * @template [T = unknown]
+ * @this {Source<T>}
+ * @returns {T | null | undefined}
+ */
+function getSourceValue() {
+  if (activeNode && activeNode.callback) {
+    const sourceSlot = this.nodes?.length || 0,
+      nodeSlot = activeNode.sources?.length || 0;
+    if (activeNode.sources === null) {
+      activeNode.sources = [this];
+      activeNode.sourceSlots = [sourceSlot];
+    } else {
+      activeNode.sources.push(this);
+      activeNode.sourceSlots.push(sourceSlot);
+    }
+    if (this.nodes === null) {
+      this.nodes = [activeNode];
+      this.nodeSlots = [nodeSlot];
+    } else {
+      this.nodes.push(activeNode);
+      this.nodeSlots.push(nodeSlot);
+    }
+  }
+  return this.value;
+}
+
+/**
+ * @template [T = unknown]
+ * @this {Source<T>}
+ * @param {T | ((value: T) => T)} value
+ * @returns {void}
+ */
+function setSourceValue(value) {
+  if (typeof value === "function") value = value(this.value);
+  this.value = value;
+  queueSourceNodes.call(this);
+}
+
+/**
+ * @template [T = unknown]
+ * @this {Source<T>}
+ * @returns {void}
+ */
+function queueSourceNodes() {
+  if (this.nodes?.length) {
+    batch(() => {
+      for (const node of this.nodes) {
+        Queue.add(node);
+      }
+    });
+  }
+}
+
+/**
+ * @template [T = unknown]
+ * @this {Source<T>}
+ * @param {T | ((value: T) => T)} [value]
+ * @returns {T | void}
+ */
+function sourceValue(value) {
+  return arguments.length === 1
+    ? setSourceValue.call(this, value)
+    : getSourceValue.call(this);
+}
+
+/**
+ * @template [T = unknown]
+ * @param {T} [initialValue]
+ * @returns {Signal<T>}
+ */
+export function signal(initialValue) {
+  return sourceValue.bind(createSource(initialValue));
+}
+
+/**
+ * @template [T = unknown]
+ * @param {T} [initialValue]
+ * @returns {Ref<T>}
+ */
+export function ref(initialValue) {
+  const source = createSource(initialValue);
+  return {
+    get value() {
+      return getSourceValue.call(source);
+    },
+    set value(nextValue) {
+      setSourceValue.call(source, nextValue);
+    },
+  };
+}
+
+/**
+ * @template [T = unknown]
+ * @param {T & object} object
+ * @returns {T}
+ */
+export function reactive(object) {
+  return new Proxy(createSource(object), reactiveTrap);
+}
+
+/** @type {ProxyHandler<Source<T & object>>)} */
+const reactiveTrap = {
+  set(target, property, value) {
+    const reflection = Reflect.set(target.value, property, value);
+    queueSourceNodes.call(target);
+    return reflection;
+  },
+  get(target, property) {
+    return Reflect.get(getSourceValue.call(target), property);
+  },
+  has(target, property) {
+    return Reflect.has(getSourceValue.call(target), property);
+  },
+};
+
+/**
+ * @param {any} error
+ * @returns {void}
+ */
+function handleError(error) {
+  /** @type {((err: any) => void)[]} */
+  const errorCallbacks = lookup.call(activeNode, Error);
+  if (!errorCallbacks) {
+    return reportError(error);
+  }
+  for (const callback of errorCallbacks) {
+    callback(error);
+  }
+}
+
+/**
+ * @template [T = unknown]
+ * @param {(error: T) => void} callback
+ * @returns {void}
+ */
+export function catchError(callback) {
+  if (activeNode === null) {
+    return;
+  }
+  if (activeNode.injections === null) {
+    activeNode.injections = { [Error]: [callback] };
+  } else {
+    activeNode.injections[Error].push(callback);
+  }
+}
+
+/**
+ * @param {Cleanup} callback
+ * @returns {void}
+ */
+export function onCleanup(callback) {
+  if (activeNode === null) {
+    return;
+  }
+  if (activeNode.cleanups === null) {
+    activeNode.cleanups = [callback];
+  } else {
+    activeNode.cleanups.push(callback);
+  }
+}
+
+/**
+ * @template [T = unknown]
+ * @param {() => T} callback
+ * @returns {T}
+ */
+export function untrack(callback) {
+  const node = activeNode;
+  activeNode = null;
+  const result = callback();
+  activeNode = node;
+  return result;
+}
+
+/**
+ * @template [T = unknown]
+ * @param {() => T} callback
+ * @returns {T}
+ */
+function batch(callback) {
+  if (isRunning) {
+    return callback();
+  }
+  isRunning = true;
+  const result = callback();
+  queueMicrotask(flush);
+  return result;
+}
+
+/**
+ * @returns {void}
+ */
+function flush() {
+  if (isRunning === false) {
+    return;
+  }
+  for (const node of Queue) {
+    Queue.delete(node);
+    updateNode.call(node, false);
+  }
+  isRunning = false;
+}
+
+/**
+ * @this {Node}
+ * @param {boolean} complete
+ * @returns {void}
+ */
+function updateNode(complete) {
+  cleanNode.call(this, complete);
+  if (this.callback === null) {
+    return;
+  }
+  const previousNode = activeNode;
+  activeNode = this;
+  try {
+    this.value = this.callback(this.value);
+  } catch (error) {
+    handleError(error);
+  } finally {
+    activeNode = previousNode;
+  }
+}
+
+/**
+ * @this {Node}
+ */
+function cleanNodeSources() {
+  while (this.sources.length) {
+    const source = this.sources.pop();
+    const sourceSlot = this.sourceSlots.pop();
+    if (source.nodes?.length) {
+      const sourceNode = source.nodes.pop();
+      const nodeSlot = source.nodeSlots.pop();
+      if (sourceSlot < source.nodes.length) {
+        source.nodes[sourceSlot] = sourceNode;
+        source.nodeSlots[sourceSlot] = nodeSlot;
+        sourceNode.sourceSlots[nodeSlot] = sourceSlot;
+      }
+    }
+  }
+}
+
+/**
+ * @this {Node}
+ * @param {boolean} complete
+ * @returns {void}
+ */
+function cleanChildNodes(complete) {
+  const hasCallback = !!this.callback;
+  while (this.children.length) {
+    const childNode = this.children.pop();
+    cleanNode.call(
+      childNode,
+      complete || (hasCallback && !!childNode.callback),
+    );
+  }
+}
+
+/**
+ * @this {Node}
+ * @param {boolean} complete
+ * @returns {void}
+ */
+function cleanNode(complete) {
+  if (this.sources?.length) {
+    cleanNodeSources.call(this);
+  }
+  if (this.children?.length) {
+    cleanChildNodes.call(this, complete);
+  }
+  if (this.cleanups?.length) {
+    cleanup.call(this);
+  }
+  this.injections = null;
+  if (complete) {
+    disposeNode.call(this);
+  }
+}
+
+/**
+ * @this {Node}
+ * @returns {void}
+ */
+function cleanup() {
+  while (this.cleanups?.length) {
+    this.cleanups.pop()();
+  }
+}
+
+/**
+ * @this {Node}
+ * @returns {void}
+ */
+function disposeNode() {
+  this.value = null;
+  this.parentNode = null;
+  this.children = null;
+  this.cleanups = null;
+  this.callback = null;
+  this.sources = null;
+  this.sourceSlots = null;
+}
+
+/**
+ * @template [T = unknown]
+ * @param {T} [defaultValue]
+ * @returns {Injection<T>}
+ */
+export function injection(defaultValue) {
+  return {
+    id: Symbol(),
+    defaultValue,
+    provide(value, callback) {
+      return scoped(() => {
+        activeNode.injections = { [this.id]: value };
+        return callback();
+      });
+    },
+  };
+}
+
+/**
+ * @template [T = unknown]
+ * @param {Injection<T>} injection
+ * @returns {T | undefined}
+ */
+export function inject(injection) {
+  return lookup.call(activeNode, injection.id) || injection.defaultValue;
+}
--- mod.ts
+++ mod.ts
@@ -1,65 +1,75 @@
-export type Cleanup = () => void
-export type Signal<T = any> = {
-  (): T
-  (value: T | undefined): void
-  (callback: (current: T | undefined) => T): void
-}
-export type Source<T = any> = {
-  value: T | undefined | null
-  nodes: Node[] | null
-  nodeSlots: number[] | null
-}
-export type Node<T = any> = {
-  value: T | undefined | null
-  parentNode: Node | null
-  children: Node[] | null
-  injections: { [id: symbol]: any } | null
-  cleanups: Cleanup[] | null
-  callback: ((current: T) => T) | null
-  sources: Source[] | null
-  sourceSlots: number[] | null
-}
-export type Ref<T = any> = {
-  value: T
-}
-export type Injection<T> = {
-  readonly id: symbol
-  readonly defaultValue: T | null
-  provide<R>(value: T, callback: () => R): R
-}
+// todo: delete file
 
-const Error = Symbol()
-const Queue = new Set<Node>()
-let isRunning = false
-let activeNode: Node | null = null
+export type Cleanup = () => void;
+export type Signal<T = any> = {
+  (): T;
+  (value: T | undefined): void;
+  (callback: (current: T | undefined) => T): void;
+};
+export type Source<T = any> = {
+  value: T | undefined | null;
+  nodes: Node[] | null;
+  nodeSlots: number[] | null;
+};
+export type Node<T = any> = {
+  value: T | undefined | null;
+  parentNode: Node | null;
+  children: Node[] | null;
+  injections: { [id: symbol]: any } | null;
+  cleanups: Cleanup[] | null;
+  callback: ((current: T) => T) | null;
+  sources: Source[] | null;
+  sourceSlots: number[] | null;
+};
+export type Ref<T = any> = {
+  value: T;
+};
+export type Injection<T> = {
+  readonly id: symbol;
+  readonly defaultValue: T | undefined;
+  provide<R>(value: T, callback: () => R): R;
+};
+
+const Error = Symbol();
+const Queue = new Set<Node>();
+let isRunning = false;
+let activeNode: Node | null = null;
 
 export function scoped<T = any>(callback: (cleanup: Cleanup) => T): T | void {
-  activeNode = createNode<T>()
+  activeNode = createNode<T>();
   try {
     return batch(() => {
-      let _cleanup: Cleanup | never = <never> undefined
+      let _cleanup: Cleanup | never = <never> undefined;
       if (callback.length) {
-        _cleanup = cleanNode.bind(undefined, activeNode!, true)
+        _cleanup = cleanNode.bind(undefined, activeNode!, true);
       }
-      return callback(_cleanup)
-    })!
+      return callback(_cleanup);
+    })!;
   } catch (error) {
-    handleError(error)
+    handleError(error);
   } finally {
-    activeNode = activeNode.parentNode
+    activeNode = activeNode.parentNode;
   }
 }
 
 export function nodeRef(): Node | null {
-  return activeNode
+  return activeNode;
 }
 
-function createNode<T = any>(): Node<T | undefined>
-function createNode<T = any>(initialValue: T): Node<T>
+export function withNode<T>(node: Node, callback: () => T): T {
+  const previousNode = activeNode;
+  activeNode = node;
+  const result = callback();
+  activeNode = previousNode;
+  return result;
+}
+
+function createNode<T = any>(): Node<T | undefined>;
+function createNode<T = any>(initialValue: T): Node<T>;
 function createNode<T = any>(
   initialValue: T,
   callback: (current: T | undefined) => T,
-): Node<T>
+): Node<T>;
 function createNode(
   initialValue?: any,
   callback?: (current: any | undefined) => any,
@@ -73,23 +83,23 @@ function createNode(
     callback: callback || null,
     sources: null,
     sourceSlots: null,
-  }
+  };
   if (activeNode) {
     if (activeNode.children === null) {
-      activeNode.children = [node]
+      activeNode.children = [node];
     } else {
-      activeNode.children.push(node)
+      activeNode.children.push(node);
     }
   }
-  return node
+  return node;
 }
 
 export function onMount(callback: () => void): void {
-  effect(() => untrack(callback))
+  effect(() => untrack(callback));
 }
 
 export function onDestroy(callback: () => void): void {
-  onCleanup(() => untrack(callback))
+  onCleanup(() => untrack(callback));
 }
 
 export function on<T>(
@@ -97,41 +107,41 @@ export function on<T>(
   callback: (current: T | undefined) => T,
 ): (current: T | undefined) => T {
   return ((current) => {
-    dependency()
-    return untrack(() => callback(current))
-  })
+    dependency();
+    return untrack(() => callback(current));
+  });
 }
 
-export function effect<T>(callback: (current: T | undefined) => T): void
+export function effect<T>(callback: (current: T | undefined) => T): void;
 export function effect<T, I>(
   callback: (current: I | T) => T,
   initialValue: I,
-): void
+): void;
 export function effect(
   callback: (current: unknown) => unknown,
   initialValue?: unknown,
 ): void {
   if (activeNode) {
-    const node = createNode(initialValue, callback)
-    if (isRunning) Queue.add(node)
-    else queueMicrotask(() => updateNode(node, false))
+    const node = createNode(initialValue, callback);
+    if (isRunning) Queue.add(node);
+    else queueMicrotask(() => updateNode(node, false));
   } else {
-    queueMicrotask(() => callback(initialValue))
+    queueMicrotask(() => callback(initialValue));
   }
 }
 
-export function computed<T>(callback: (current: T | undefined) => T): () => T
+export function computed<T>(callback: (current: T | undefined) => T): () => T;
 export function computed<T, I>(
   callback: (current: I | T) => T,
   initialValue: I,
-): () => T
+): () => T;
 export function computed(
   callback: (current: unknown | undefined) => unknown,
   initialValue?: unknown,
 ): (current: unknown) => unknown {
-  const source = createSource(initialValue)
-  effect(() => setSourceValue(source, callback(source.value!)))
-  return getSourceValue.bind(undefined, source)
+  const source = createSource(initialValue);
+  effect(() => setSourceValue(source, callback(source.value!)));
+  return getSourceValue.bind(undefined, source);
 }
 
 function lookup(node: Node | null, id: symbol): any | undefined {
@@ -139,189 +149,189 @@ function lookup(node: Node | null, id: symbol): any | undefined {
     ? node.injections && id in node.injections
       ? node.injections[id]
       : lookup(node.parentNode, id)
-    : undefined
+    : undefined;
 }
 
-function createSource<T = any>(): Source<T | undefined>
-function createSource<T = any>(initialValue: T): Source<T>
+function createSource<T = any>(): Source<T | undefined>;
+function createSource<T = any>(initialValue: T): Source<T>;
 function createSource(initialValue?: any): Source<any | undefined> {
-  return { value: initialValue, nodes: null, nodeSlots: null }
+  return { value: initialValue, nodes: null, nodeSlots: null };
 }
 
 function getSourceValue<T = any>(source: Source<T>): T {
   if (activeNode && activeNode.callback) {
     const sourceSlot = source.nodes?.length || 0,
-      nodeSlot = activeNode.sources?.length || 0
+      nodeSlot = activeNode.sources?.length || 0;
     if (activeNode.sources === null) {
-      activeNode.sources = [source]
-      activeNode.sourceSlots = [sourceSlot]
+      activeNode.sources = [source];
+      activeNode.sourceSlots = [sourceSlot];
     } else {
-      activeNode.sources.push(source)
-      activeNode.sourceSlots!.push(sourceSlot)
+      activeNode.sources.push(source);
+      activeNode.sourceSlots!.push(sourceSlot);
     }
     if (source.nodes === null) {
-      source.nodes = [activeNode]
-      source.nodeSlots = [nodeSlot]
+      source.nodes = [activeNode];
+      source.nodeSlots = [nodeSlot];
     } else {
-      source.nodes!.push(activeNode)
-      source.nodeSlots!.push(nodeSlot)
+      source.nodes!.push(activeNode);
+      source.nodeSlots!.push(nodeSlot);
     }
   }
-  return source.value!
+  return source.value!;
 }
 
 function setSourceValue<T = any>(source: Source<T>, value: any): void {
-  if (typeof value === "function") value = value(source.value)
-  source.value = value
+  if (typeof value === "function") value = value(source.value);
+  source.value = value;
   if (source.nodes?.length) {
     batch(() => {
       for (const node of source.nodes!) {
-        Queue.add(node)
+        Queue.add(node);
       }
-    })
+    });
   }
 }
 
 function sourceValue<T = any>(source: Source<T>, value?: any): T | void {
   return arguments.length === 1
     ? getSourceValue(source)
-    : setSourceValue(source, value)
+    : setSourceValue(source, value);
 }
 
-export function signal<T>(): Signal<T | undefined>
-export function signal<T>(initialValue: T): Signal<T>
+export function signal<T>(): Signal<T | undefined>;
+export function signal<T>(initialValue: T): Signal<T>;
 export function signal(initialValue?: any): Signal<any | undefined> {
-  const source = createSource(initialValue)
-  return sourceValue.bind(undefined, source) as Signal<any | undefined>
+  const source = createSource(initialValue);
+  return sourceValue.bind(undefined, source) as Signal<any | undefined>;
 }
 
-export function ref<T>(): Ref<T | undefined>
-export function ref<T>(initialValue: T): Ref<T>
+export function ref<T>(): Ref<T | undefined>;
+export function ref<T>(initialValue: T): Ref<T>;
 export function ref(initialValue?: any): Ref<any | undefined> {
-  const source = createSource(initialValue)
+  const source = createSource(initialValue);
   return {
     get value() {
-      return getSourceValue(source)
+      return getSourceValue(source);
     },
     set value(nextValue) {
-      setSourceValue(source, nextValue)
+      setSourceValue(source, nextValue);
     },
-  }
+  };
 }
 
 function handleError(error: any): void {
-  const errorCallbacks: ((err: any) => void)[] = lookup(activeNode, Error)
-  if (!errorCallbacks) return reportError(error)
+  const errorCallbacks: ((err: any) => void)[] = lookup(activeNode, Error);
+  if (!errorCallbacks) return reportError(error);
   for (const callback of errorCallbacks) {
-    callback(error)
+    callback(error);
   }
 }
 
 export function catchError<T = any>(callback: (error: T) => void): void {
-  if (activeNode === null) return
+  if (activeNode === null) return;
   if (activeNode.injections === null) {
-    activeNode.injections = { [Error]: [callback] }
+    activeNode.injections = { [Error]: [callback] };
   } else {
-    activeNode.injections[Error].push(callback)
+    activeNode.injections[Error].push(callback);
   }
 }
 
 export function onCleanup(callback: () => void): void {
-  if (activeNode === null) return
-  else if (!activeNode.cleanups) activeNode.cleanups = [callback]
-  else activeNode.cleanups.push(callback)
+  if (activeNode === null) return;
+  else if (!activeNode.cleanups) activeNode.cleanups = [callback];
+  else activeNode.cleanups.push(callback);
 }
 
 export function untrack<T>(callback: () => T): T {
-  const node = activeNode
-  activeNode = null
-  const result = callback()
-  activeNode = node
-  return result
+  const node = activeNode;
+  activeNode = null;
+  const result = callback();
+  activeNode = node;
+  return result;
 }
 
 function batch<T>(callback: () => T): T {
-  if (isRunning) return callback()
-  isRunning = true
-  const result = callback()
-  queueMicrotask(flush)
-  return result
+  if (isRunning) return callback();
+  isRunning = true;
+  const result = callback();
+  queueMicrotask(flush);
+  return result;
 }
 
 function flush(): void {
-  if (isRunning === false) return
+  if (isRunning === false) return;
   for (const node of Queue) {
-    Queue.delete(node)
-    updateNode(node, false)
+    Queue.delete(node);
+    updateNode(node, false);
   }
-  isRunning = false
+  isRunning = false;
 }
 
 function updateNode(node: Node, complete: boolean): void {
-  cleanNode(node, complete)
-  if (node.callback === null) return
-  const previousNode = activeNode
-  activeNode = node
+  cleanNode(node, complete);
+  if (node.callback === null) return;
+  const previousNode = activeNode;
+  activeNode = node;
   try {
-    node.value = node.callback(node.value)
+    node.value = node.callback(node.value);
   } catch (error) {
-    handleError(error)
+    handleError(error);
   } finally {
-    activeNode = previousNode
+    activeNode = previousNode;
   }
 }
 
 function cleanNodeSources(node: Node): void {
-  let source: Source, sourceSlot: number, sourceNode: Node, nodeSlot: number
+  let source: Source, sourceSlot: number, sourceNode: Node, nodeSlot: number;
   while (node.sources!.length) {
-    source = node.sources!.pop()!
-    sourceSlot = node.sourceSlots!.pop()!
+    source = node.sources!.pop()!;
+    sourceSlot = node.sourceSlots!.pop()!;
     if (source.nodes?.length) {
-      sourceNode = source.nodes.pop()!
-      nodeSlot = source.nodeSlots!.pop()!
+      sourceNode = source.nodes.pop()!;
+      nodeSlot = source.nodeSlots!.pop()!;
       if (sourceSlot < source.nodes.length) {
-        source.nodes[sourceSlot] = sourceNode
-        source.nodeSlots![sourceSlot] = nodeSlot
-        sourceNode.sourceSlots![nodeSlot] = sourceSlot
+        source.nodes[sourceSlot] = sourceNode;
+        source.nodeSlots![sourceSlot] = nodeSlot;
+        sourceNode.sourceSlots![nodeSlot] = sourceSlot;
       }
     }
   }
 }
 
 function cleanChildNodes(node: Node, complete: boolean): void {
-  const hasCallback = node.callback !== undefined
-  let childNode: Node
+  const hasCallback = node.callback !== undefined;
+  let childNode: Node;
   while (node.children!.length) {
-    childNode = node.children!.pop()!
+    childNode = node.children!.pop()!;
     cleanNode(
       childNode,
       complete || (hasCallback && childNode.callback !== undefined),
-    )
+    );
   }
 }
 
 function cleanNode(node: Node, complete: boolean): void {
-  if (node.sources?.length) cleanNodeSources(node)
-  if (node.children?.length) cleanChildNodes(node, complete)
-  if (node.cleanups?.length) cleanup(node)
-  node.injections = null
-  if (complete) disposeNode(node)
+  if (node.sources?.length) cleanNodeSources(node);
+  if (node.children?.length) cleanChildNodes(node, complete);
+  if (node.cleanups?.length) cleanup(node);
+  node.injections = null;
+  if (complete) disposeNode(node);
 }
 
 function cleanup(node: Node): void {
   while (node.cleanups?.length) {
-    node.cleanups.pop()!()
+    node.cleanups.pop()!();
   }
 }
 
 function disposeNode(node: Node): void {
-  node.value = null
-  node.parentNode = null
-  node.children = null
-  node.cleanups = null
-  node.callback = null
-  node.sources = null
-  node.sourceSlots = null
+  node.value = null;
+  node.parentNode = null;
+  node.children = null;
+  node.cleanups = null;
+  node.callback = null;
+  node.sources = null;
+  node.sourceSlots = null;
 }
 
 export function injection<T>(defaultValue?: T): Injection<T | undefined> {
@@ -330,9 +340,9 @@ export function injection<T>(defaultValue?: T): Injection<T | undefined> {
     defaultValue,
     provide(value, callback) {
       return scoped(() => {
-        activeNode!.injections = { [this.id]: value }
-        return callback()
-      })!
+        activeNode!.injections = { [this.id]: value };
+        return callback();
+      })!;
     },
-  }
+  };
 }
