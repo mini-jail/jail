eac388fd92417aad022a6145fff1f879876768f4
mini-jail <mini-jail@proton.me>
2023-07-17 13:02:18 +0200

    <3

--- examples/router.js
+++ examples/router.js
@@ -2,44 +2,47 @@ import { onCleanup, onMount } from "jail/signal"
 import { createRouter, path } from "jail/router"
 import { createComponent } from "jail/dom"
 
-export function installRouter() {
-  createComponent("Router", ({ type, children, fallback, routes }) => {
-    const router = createRouter(routes, { fallback })
-    let listener, accessor, eventName
+function useRouter(type) {
+  let listener, accessor, eventName
 
-    if (type === "hash") {
-      eventName = "hashchange"
-      accessor = () => location.hash.slice(1) || "/"
-      listener = () => path(accessor())
-    }
+  if (type === "hash") {
+    eventName = "hashchange"
+    accessor = () => location.hash.slice(1) || "/"
+    listener = () => path(accessor())
+  }
 
-    if (type === "pathname") {
-      eventName = "click"
-      accessor = () => location.pathname
-      listener = (event) => {
-        event.preventDefault()
-        let elt = event.target, pathname = null
-        while (elt !== null) {
-          pathname = elt.getAttribute?.("href") || null
-          if (pathname != null) {
-            break
-          }
-          elt = elt.parentNode
+  if (type === "pathname") {
+    eventName = "click"
+    accessor = () => location.pathname
+    listener = (event) => {
+      event.preventDefault()
+      let elt = event.target, pathname = null
+      while (elt !== null) {
+        pathname = elt.getAttribute?.("href") || null
+        if (pathname !== null) {
+          break
         }
-        if (pathname !== null && pathname !== accessor()) {
-          path(pathname)
-          history.pushState(null, "", pathname)
-        }
+        elt = elt.parentNode
+      }
+      if (pathname !== null && pathname !== accessor()) {
+        path(pathname)
+        history.pushState(null, "", pathname)
       }
     }
+  }
 
-    onMount(() => {
-      path(accessor())
-      addEventListener(eventName, listener)
-    })
+  onMount(() => {
+    path(accessor())
+    addEventListener(eventName, listener)
+  })
 
-    onCleanup(() => removeEventListener(eventName, listener))
+  onCleanup(() => removeEventListener(eventName, listener))
+}
 
-    return children ? [children, router] : router
+export function installRouter() {
+  createComponent("Router", (props) => {
+    const router = createRouter(props.routes, { fallback: props.fallback })
+    useRouter(props.type)
+    return props.children ? [props.children, router] : router
   })
 }
