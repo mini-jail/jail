cb57d01e3962e60e52bcef409f4b6e01b30a596a
mini-jail <mini-jail@proton.me>
2023-07-20 16:12:59 +0200

    add dev mode

--- bundle.ts
+++ bundle.ts
@@ -3,12 +3,31 @@ import {
   type BundleOptions,
 } from "https://deno.land/x/emit@0.24.0/mod.ts"
 
-const [sourceFile, targetFile, importMap = "./import_map.json"] = Deno.args
+const [
+  sourceFile,
+  targetFile = sourceFile + ".bundle.js",
+  mode = "not_dev",
+  importMap = "./import_map.json",
+] = Deno.args
 const options: BundleOptions = { importMap }
-const { code } = await bundle(sourceFile, options)
+const root = sourceFile.split("/").at(-2)!
 
-if (targetFile) {
-  await Deno.writeTextFile(targetFile, code)
+if (mode === "dev") {
+  let built = false
+  await createBundle()
+  for await (const { kind } of Deno.watchFs([root, sourceFile])) {
+    if (["access", "other"].includes(kind)) continue
+    else if (built === true) continue
+    await createBundle()
+    built = true
+    setTimeout(() => built = false, 500)
+  }
 } else {
-  console.log(code)
+  await createBundle()
+}
+
+async function createBundle() {
+  console.log(`creating "${targetFile}"`)
+  const { code } = await bundle(sourceFile, options)
+  await Deno.writeTextFile(targetFile, code)
 }
