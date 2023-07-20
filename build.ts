650b2942c55a9adaa22f0440e3ac01826b214cab
mini-jail <mini-jail@proton.me>
2023-07-20 15:47:00 +0200

    mhm yes

--- build.ts
+++ build.ts
@@ -9,22 +9,22 @@ const fileMap = new Map<string, string>()
 const root = "./packages/"
 
 for await (const entry of Deno.readDir(root)) {
-  if (entry.isDirectory) {
-    for await (const packageEntry of Deno.readDir(root + entry.name)) {
-      if (packageEntry.name.endsWith(".ts") === false) {
-        continue
-      }
-      const fileName = root + `${entry.name}/${packageEntry.name}`
-      const result = await transpile(fileName, options)
-      for (const key of result.keys()) {
-        const path = key.split("/")
-        const fileName = path.at(-1)!
-        const targetName = fileName.replace(".ts", ".js")
-        const packageName = path.at(-2)!
-        let data = `/// <reference types="./${fileName}" />\n`
-        data = data + result.get(key)
-        fileMap.set(root + `${packageName}/${targetName}`, data)
-      }
+  if (entry.isDirectory === false) {
+    continue
+  }
+  for await (const { name, isFile } of Deno.readDir(root + entry.name)) {
+    if (name.endsWith(".ts") === false || isFile === false) {
+      continue
+    }
+    const packageModule = root + `${entry.name}/${name}`
+    const result = await transpile(packageModule, options)
+    for (const key of result.keys()) {
+      const path = key.split("/")
+      const fileName = path.at(-1)!
+      const packageName = path.at(-2)!
+      const targetFileName = fileName.replace(".ts", ".js")
+      const data = `/// <reference types="./${fileName}" />\n` + result.get(key)
+      fileMap.set(root + `${packageName}/${targetFileName}`, data)
     }
   }
 }
