684b509d92ca348ca127f5c78ac9057a1e558021
mini-jail <mini-jail@proton.me>
2023-07-20 07:30:40 +0200

    oompa loompa

--- examples/app.d.ts
+++ examples/app.d.ts
@@ -1,8 +1,12 @@
+export interface AnimateDirective {
+  frames: Keyframe[]
+  options: KeyframeAnimationOptions
+}
+
 declare global {
   namespace jail {
-    interface ExtendableDirectiveMap {
-      animate: { frames: Keyframe[]; options: KeyframeAnimationOptions }
+    interface Directives {
+      animate: AnimateDirective
     }
   }
 }
-export {}
